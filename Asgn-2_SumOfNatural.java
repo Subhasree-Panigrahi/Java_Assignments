b2be75ae70115f6ca5323acad23452be0bebf8f3
Subhasree-Panigrahi <141422483+Subhasree-Panigrahi@users.noreply.github.com>
2025-01-03 09:20:18 +0530

    Update Asgn-2_SumOfNatural.java

--- Asgn-2_SumOfNatural.java
+++ Asgn-2_SumOfNatural.java
@@ -1,3 +1,5 @@
+//Exercise 1: Create a class with a method which can calculate the sum of first n natural 
+//numbers which are divisible by 3 or 5.
 package cts.natural;
 import java.util.Scanner;
 

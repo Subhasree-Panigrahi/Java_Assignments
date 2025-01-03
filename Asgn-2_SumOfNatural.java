67e516cb541227b7a2eb6669816ee672d8d6b009
Subhasree-Panigrahi <141422483+Subhasree-Panigrahi@users.noreply.github.com>
2025-01-03 09:16:04 +0530

    Create Asgn-2_SumOfNatural.java

---
+++ Asgn-2_SumOfNatural.java
@@ -0,0 +1,21 @@
+package cts.natural;
+import java.util.Scanner;
+
+public class SumOfNatural {
+	public int calculateSum(int n) {
+		int sum=0;
+		for (int i=0;i<=n;i++) {
+			if (i%3==0 || i%5==0) {
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		SumOfNatural son=new SumOfNatural();
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int s=son.calculateSum(num);
+		System.out.println("Sum of first "+num+ " natural numbers which are divisible by 3 or 5 is: "+s);
+	}
+}

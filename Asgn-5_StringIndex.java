86d9306eeef34bdae99f9a15d675d7c1566982a9
Subhasree-Panigrahi <141422483+Subhasree-Panigrahi@users.noreply.github.com>
2025-01-12 16:34:18 +0530

    Create Asgn-5_StringIndex.java

---
+++ Asgn-5_StringIndex.java
@@ -0,0 +1,10 @@
+//1) Write a Java program to get the character at the given index within the String
+
+package com.cts.asgn_5;
+
+public class StringIndex {
+	public static void main(String[] args) {
+		String s="Subhasree Panigrahi";
+		System.out.println(s.charAt(10));
+	}
+}
